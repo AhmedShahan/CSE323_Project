21519fa07ac8ff8a5ed846a84a233f67710f1e76
AhmedShahan <shahan.ahmed001@gmail.com>
2022-11-03 11:23:22 +0600

    UI and Adding process

--- home.py
+++ home.py
@@ -1,14 +1,34 @@
-<<<<<<< HEAD
-=======
 from PySimpleGUI import *
 
+theme("DarkGreen4")
+theme_button_color("black")
+theme_input_background_color("white")
+theme_input_text_color("#001D3C")
+
+algorithm=["FCFS","SJF","Priority","Round Robin"]
+
+process=[]
 layout=[
-[Text("Welcome to Schedule Management")],
+[Text("Welcome to Schedule Management",font=("Jokerman",40,"underline"),size=(1200,1),justification="c")],
+[Text("Select Algorithm: ",font=("Tomes & New Roma",15,"bold")),Combo(values=algorithm)],
+[Text("Enter Process ",font=("Tomes & New Roma",15,"bold")),InputText(key="proc")],
+[Button("+ADD+",key="addP")],
+
+
+
 
 ]
-Window("Schedule Management",layout=layout).read()
+doc = Window('Schedule Algorith Checker', size=(1200,750),location=(100,10),return_keyboard_events=True).Layout(layout)
 
-
-
-# Developed by
->>>>>>> temp
+while True:
+    event,value=doc.read()
+    if event == WIN_CLOSED:
+            break
+    
+    if event=="addP":
+        pvalue=value["proc"]
+        if len(pvalue)==0:
+            popup("Please Enter Process Name")
+        else:
+            popup("Process Added")
+            process.append(pvalue)
