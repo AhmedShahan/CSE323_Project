266e7a427f85ec0e0d8d8c32ff224af035af3cf5
AhmedShahan <shahan.ahmed001@gmail.com>
2022-11-03 12:29:34 +0600

    Process Details in UI

--- home.py
+++ home.py
@@ -11,10 +11,13 @@ process=[]
 layout=[
 [Text("Welcome to Schedule Management",font=("Jokerman",40,"underline"),size=(1200,1),justification="c")],
 [Text("Select Algorithm: ",font=("Monotype Corsiva",15,"bold"),size=(17,1)),Combo(values=algorithm)],
-[Text("Enter Process ",font=("Monotype Corsiva",15,"bold"),size=(17,1)),InputText(key="proc")],
-[Button("+ADD+",key="addP")],
+[Text("Enter Process Details ",font=("Monotype Corsiva",15,"bold","underline"),size=(10,1))],
+[Text("Process Name: ",font=("Monotype Corsiva",15,"bold"),size=(10,1)),InputText(key="proc",size=(5,1))],
+[Text("Arrivel Time",font=("Monotype Corsiva",15,"bold"),size=(10,1)),InputText(key="at",size=(5,1)),Checkbox("Default: 0",key="default")],
+[Text("Burst Time: ",font=("Monotype Corsiva",15,"bold"),size=(10,1)),InputText(key="bt",size=(5,1))],
+[Text('\t\t\t'*2),Button("+ADD+",key="addP")],
 
-[Text('\n\n\n'),Text("Process List: "),Input(key="output")],
+# [Text("Enter Process ",font=("Monotype Corsiva",15,"bold"),size=(10,1)),Input(key="output",size=(5,1))],
 
 
 
