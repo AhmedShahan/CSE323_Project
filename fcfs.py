43f6dba1d80ace3d9e65692839ffaeffe66add71
AhmedShahan <shahan.ahmed001@gmail.com>
2022-12-08 14:14:57 +0600

    fcfs slightly changes for status bar

--- fcfs.py
+++ fcfs.py
@@ -4,40 +4,41 @@
 # p3
 
 # Process Birstime Allocation
-a=[("p4",10,2),("p3",3,5),("p1",7,2),("p2",9,0),("p5",12,6)]
-# print("Process|\tAllocation Time|Burst Time")
+def algorithm():
+    a=[("4",10,2),("3",3,5),("1",7,2),("2",9,0),("5",12,6)]
+    # print("Process|\tAllocation Time|Burst Time")
 
-# for i in range(0, len(a)):
-#     for j in range(0,3):
-#         print(a[i][j],end="\t")
-#     print('\n')
+    # for i in range(0, len(a)):
+    #     for j in range(0,3):
+    #         print(a[i][j],end="\t")
+    #     print('\n')
 
-a.sort(key = lambda x : x[0])
+    a.sort(key = lambda x : x[0])
 
-a.sort(key = lambda x : x[2])
+    a.sort(key = lambda x : x[2])
+    print(a)
 
-print(a)
 
+    allocation=[]
 
-allocation=[]
+    #first allocation time in gang chart
+    first=a[0][2]
+    allocation.append(first)
 
-#first allocation time in gang chart
-first=a[0][2]
-allocation.append(first)
-
-second=a[0][2] + a[0][1]
-allocation.append(second)
-
-for i in range(1,len(a)):
-    second=second+a[i][1]
+    second=a[0][2] + a[0][1]
     allocation.append(second)
 
-print(allocation)
+    for i in range(1,len(a)):
+        second=second+a[i][1]
+        allocation.append(second)
 
-sum=0
-for i in range(0,len(a)):
-    sum=sum+(allocation[i]-a[i][2])
+    print(allocation)
 
-awt=float(sum)/len(a)
+    sum=0
+    for i in range(0,len(a)):
+        sum=sum+(allocation[i]-a[i][2])
 
-print(awt)
+    awt=float(sum)/len(a)
+
+    print(awt)
+    return a
