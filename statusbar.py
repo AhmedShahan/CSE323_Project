ccd55baf561bdb96340eb8052376538254b0bcc4
AhmedShahan <shahan.ahmed001@gmail.com>
2022-12-08 14:31:45 +0600

    statusbar for fcfs done

--- statusbar.py
+++ statusbar.py
@@ -4,35 +4,25 @@ import time
 import math
 theme('pythonPlus')
 layout=[
-    [ProgressBar(100,orientation='h',size=(50,3),border_width=4,key="progressBar0", bar_color=("Black","white")),Input(key='value0',size=(5,1))],
     [ProgressBar(100,orientation='h',size=(50,3),border_width=4,key="progressBar1", bar_color=("Black","white")),Input(key='value1',size=(5,1))],
     [ProgressBar(100,orientation='h',size=(50,3),border_width=4,key="progressBar2", bar_color=("Black","white")),Input(key='value2',size=(5,1))],
     [ProgressBar(100,orientation='h',size=(50,3),border_width=4,key="progressBar3", bar_color=("Black","white")),Input(key='value3',size=(5,1))],
     [ProgressBar(100,orientation='h',size=(50,3),border_width=4,key="progressBar4", bar_color=("Black","white")),Input(key='value4',size=(5,1))],
+    [ProgressBar(100,orientation='h',size=(50,3),border_width=4,key="progressBar5", bar_color=("Black","white")),Input(key='value5',size=(5,1))],
     [Button("Display Progress",key="change")],
 ]
 
 win=Window("progress Bar",layout)
 
-#For process 0
-def statusvalue0(bt):
-    BT=bt
-    if event=="change":
-        val=math.ceil(((100/BT)*BT))
-        for i in range(BT,val+1):
-            time.sleep(0.05)
-            win["progressBar0"].update(i)
-            win["value0"].update(i)
-#process 1
+#For process 1
 def statusvalue1(bt):
     BT=bt
     if event=="change":
         val=math.ceil(((100/BT)*BT))
         for i in range(BT,val+1):
-            time.sleep(0.01)
+            time.sleep(0.05)
             win["progressBar1"].update(i)
             win["value1"].update(i)
-
 #process 2
 def statusvalue2(bt):
     BT=bt
@@ -42,6 +32,8 @@ def statusvalue2(bt):
             time.sleep(0.01)
             win["progressBar2"].update(i)
             win["value2"].update(i)
+
+#process 3
 def statusvalue3(bt):
     BT=bt
     if event=="change":
@@ -58,6 +50,14 @@ def statusvalue4(bt):
             time.sleep(0.01)
             win["progressBar4"].update(i)
             win["value4"].update(i)
+def statusvalue5(bt):
+    BT=bt
+    if event=="change":
+        val=math.ceil(((100/BT)*BT))
+        for i in range(BT,val+1):
+            time.sleep(0.01)
+            win["progressBar5"].update(i)
+            win["value5"].update(i)
 
 while True:
     event,value=win.read()
@@ -66,8 +66,6 @@ while True:
 
     result=fcfs.algorithm()
     for i in result:
-        if (i[0])=='0':
-            statusvalue0(i[1])
         if (i[0])=='1':
             statusvalue1(i[1])
         if (i[0])=='2':
@@ -76,6 +74,8 @@ while True:
             statusvalue3(i[1])
         if (i[0])=='4':
             statusvalue4(i[1])
+        if (i[0])=='5':
+            statusvalue5(i[1])
 
 
 
