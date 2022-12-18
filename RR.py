a8046e0aba6489aa9fdcfdbb721b1d9b83dfacf6
myname <waziha4@gmail.com>
2022-12-18 19:39:36 +0600

    RR gui done

---
+++ RR.py
@@ -0,0 +1,73 @@
+def RRalgorithm(a,tq):
+    # Process BT AT TQ
+    TQ=tq
+    a=[('p1',3,2),('p2',5,0),('p3',3,3),('p4',6,1)]
+    OBT=[]
+    OLST=[]
+
+    # Collect all the Burst Time
+    for i in a:
+        OBT.append(i[1])
+        OLST.append(i[0])
+
+    # Sort using Arrival Time
+    a.sort(key = lambda x : x[2])
+    list1=[]
+    sortedOBT=[]
+    sortedP=[]
+    for i in a:
+        sortedOBT.append(i[1])
+        sortedP.append(i[0])
+
+    # print(OBT)
+    # print(sortedOBT)
+    # print(a)
+    templist=[]
+    maxCycle=[]
+    for i in a:
+        # print(i[1])
+        value=i[1]
+        process=i[0]
+        t=value//TQ #2
+        maxCycle.append(t)
+        r=value%TQ  #1
+        for i in range(0,t):
+            templist.append((process,TQ))
+        if r!=0:
+            templist.append((process,r))
+        
+    Output = {}
+    for x, y in templist:
+        if x in Output:
+            Output[x].append((x, y))
+        else:
+            Output[x] = [(x, y)]
+    # print(Output)
+    mainList=[]
+    maxTuple=max(maxCycle)
+    list1.append(Output.get(sortedP[0]))
+    list1.append(Output.get(sortedP[1]))
+    list1.append(Output.get(sortedP[2]))
+    list1.append(Output.get(sortedP[3]))
+
+    for i in list1:
+        if len(i)<maxTuple:
+            for j in range(0,maxTuple-len(i)):
+                i.append(("0",0))
+
+    for i in range(0,maxTuple):
+        for j in range(0,len(OBT)):
+            temp=list1[j][i]
+            if temp==("0",0):
+                pass
+            else:
+                mainList.append(list1[j][i])
+    #print(mainList)
+    return mainList
+
+
+
+    
+
+
+
--- RRstatus.py
+++ RRstatus.py
@@ -20,8 +20,6 @@ win=Window("progress Bar",layout)
 
 #For process 1
 def statusvalue1(initial,OBT,duration):
-    print("Initial",initial)
-    print("Duration",duration)
     initialP=math.ceil(((initial/OBT)*100))
     if event=="change":
         val=math.ceil(((duration/OBT)*100))
@@ -30,9 +28,6 @@ def statusvalue1(initial,OBT,duration):
             win["progressBar1"].update(i)
             win["value1"].update(i)
 
-# [('p1',0,2),('p2',0,2),('p1',2,2),('p2',2,1)]
-# statusvalue1(i[1],bt[0],i[2])
-#process 2
 def statusvalue2(initial,OBT,duration):
     initialP=math.ceil(((initial/OBT)*100))
     if event=="change":
@@ -41,67 +36,23 @@ def statusvalue2(initial,OBT,duration):
             time.sleep(0.05)
             win["progressBar2"].update(i)
             win["value2"].update(i)
-            
-            
-
-# #process 3
-# def statusvalue3(initial,OBT,duration):
-#     if event=="change":
-
-#         count=int((initial/OBT)*100)
-#         val=math.ceil(((duration/OBT)*100))
-#         for i in range(count,val+1):
-#             time.sleep(0.05)
-#             win["progressBar3"].update(i)
-#             win["value3"].update(i)
-        
-
-# def statusvalue4(initial,OBT,duration):
-#     if event=="change":
-#         count=int((initial/OBT)*100)
-#         val=math.ceil(((duration/OBT)*100))
-#         for i in range(count,val+1):
-#             time.sleep(0.05)
-#             win["progressBar4"].update(i)
-#             win["value4"].update(i)
-            
-
-# def statusvalue5(initial,OBT,duration):
-#     if event=="change":
-#         count=int((initial/OBT)*100)
-#         val=math.ceil(((duration/OBT)*100))
-#         for i in range(count,val+1):
-#             time.sleep(0.05)
-#             win["progressBar5"].update(i)
-#             win["value5"].update(i)
 
 while True:
     event,value=win.read()
     if event=="Exit" or event==WIN_CLOSED:
         break
     bt=[8,5]
-    result= [('p1',0,2),('p2',0,2),('p1',2,2),('p2',2,2),('p2',4,1),('p1',4,2),('p1',6,2)]
+    result= [('p1',2),('p2',2),('p1',2),('p2',2),('p2',1),('p1',2),('p1',2)]
+    initial1=0
+    initial2=0
     for i in result:
-        # print(i)
-        start=i[1]
-        start=int(start)
         if i[0]=='p1':
-            duration=i[1]+i[2]
-            #print(duration)
+            start=initial1
+            duration=initial1+i[1]
             statusvalue1(start,bt[0],duration)
-            print(start)
-            start=start+duration
-            print(start)
-        if i[0]=='p2':
-            duration=i[1]+i[2]
-            #print(duration)
+            initial1=duration
+        elif i[0]=='p2':
+            start=initial2
+            duration=initial2+i[1]
             statusvalue2(start,bt[1],duration)
-            print(start)
-            start=start+duration
-            print(start)
-        # if i[0]=='p3':
-        #     statusvalue3(i[1],bt[0],i[2])
-        # if i[0]=='p4':
-        #     statusvalue4(i[1],bt[0],i[2])
-        # if i[0]=='p5':
-        #     statusvalue5(i[1],bt[0],i[2])
+            initial2=duration
